1005a0a3fa81e5b0b82f77974aadaa540d56a0c8
Ethan <allric0922@gmail.com>
2019-07-28 15:13:36 -0400

    Questions added. ToDo: style style style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,55 @@
 var questions = [
     {
-        question: "First letter of Alphabet", //string that holds the question
+        question: "Dungeons and Dragons was first published in?", //string that holds the question
         correctNum: 1,  //used to set the answer in the nextQuestion() 
-        answerChoices: ["b", "a" ,"c", "d"] // all the options the correct answer is in the spot equal to the number above 
+        answerChoices: ["1981", "1974" ,"1989", "1965"] // all the options the correct answer is in the spot equal to the number above 
 
     },
     {
-        question: "The Sky is...",
-        correctNum: 2,
-        answerChoices: ["red", "pizza", "blue" ,"black"]
+        question: "Who created D&D",
+        correctNum: 0,
+        answerChoices: ["Gary Gygax", "Bill Gates", "J. R. R. Tolkin" ,"Guru Darji"]
     },
     {
-        question: "Apples are a type of...",
+        question: "Which class was not one of the three orignal",
+        correctNum: 2,
+        answerChoices: ["Magic-User", "Fighting-Man", "Druid" ,"Cleric"]
+    },
+    {
+        question: "Which was not an original race",
         correctNum: 0,
-        answerChoices: ["fruit", "horse" , "dog" , "cow"]
-    }
+        answerChoices: ["Tiefling", "Human" , "Dwarf" , "Halfling"]
+    },
+    {
+        question: "Which is the most current edition of D&D",
+        correctNum: 3,
+        answerChoices: ["3rd", "3.5/Pathfinder", "6th" ,"5th"]
+    },
+    {
+        question: "What company has the rights and coninues to publish the books",
+        correctNum: 2,
+        answerChoices: ["Sorcerers of the Sea", "Mages of the Mountains", "Wizards of the Coast" ,"Amazon"]
+    },
+    {
+        question: "Which is not an ability in D&D 5e",
+        correctNum: 1,
+        answerChoices: ["Strength", "Power", "Dexterity" ,"Charisma"]
+    },
+    {
+        question: "Who, according to D&D lore, is the lord of Nine Hells",
+        correctNum: 0,
+        answerChoices: ["Azmodeus", "Baalzebul", "Tarrasque" ,"Tiamat"]
+    },
+    {
+        question: "Which is not a skill in 5e",
+        correctNum: 3,
+        answerChoices: ["Insight", "Arcana", "Nature" ,"Smithing"]
+    },
+    {
+        question: "How much gold is a platinum piece worth",
+        correctNum: 2,
+        answerChoices: ["100", "500", "10" ,"5"]
+    },
 
 ]
 // stats
@@ -57,6 +92,7 @@ function nextQuestion() {
     $("#answers").empty()
     
     number = 20
+    $("#timer").text("Time: " + number)
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000)
 
@@ -128,19 +164,23 @@ $(document).on("click", ".guess" ,  function(){
 function clearScrn() {
     $("#answers").empty()
     $("#question").empty()
+    $("#timer").empty()
 }
 
 //function for the core timer
 function decrement() {
     number--
-    $("#timer").show().text("Time: " + number)
+    $("#timer").text("Time: " + number)
     console.log(number)
     if (number === 0){
-        
+    
+        clearInterval(intervalId)
+    setTimeout(transitionScrn, 2000)
     notAns++
 
     clearScrn()
     $("#question").append("<h1> Time's Up! </h1>")
     $("#answers").append("<h3> The Correct Answer Was: " + x + "</h3>")
+
     }
 }
